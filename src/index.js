6585bf54076e638767f22244a4c5ca65bb2c2be4
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-20 12:00:01 -0800

    add error handling for when currency selections are blank.

--- src/index.js
+++ src/index.js
@@ -9,21 +9,30 @@ const getConversionFrom = async () => {
     const fromAmount = document.getElementById("from-amount").value;
     const toCurrency = document.getElementById("to-currency").value;
     const toAmount = document.getElementById("to-amount");
+    const errorContainer = document.getElementById("error-container");
+    const errorMessage = document.createElement("h3");
 
-    const response = await ConvertCurrency.getConversion(fromCurrency);
-
-    if (response.conversion_rates) {
-        const conversionRate = response.conversion_rates[toCurrency];
-        toAmount.value = (fromAmount * conversionRate);
-    }
-
-    else {
-        console.error("Error in getConversion:", response);
-        const errorContainer = document.getElementById("error-container");
-        const errorMessage = document.createElement("h3");
-        errorMessage.innerText = `${response}. We are unable to retrieve conversion rates.`;
+    if (!fromCurrency || !toCurrency) {
+        errorMessage.innerText = "Please enter to currencies to exchange between.";
         errorContainer.innerText = "";
         errorContainer.append(errorMessage);
+
+    } else {
+
+        const response = await ConvertCurrency.getConversion(fromCurrency);
+
+        if (response.conversion_rates) {
+            const conversionRate = response.conversion_rates[toCurrency];
+            toAmount.value = (fromAmount * conversionRate);
+        }
+
+        else {
+            console.error("Error in getConversion:", response);
+
+            errorMessage.innerText = `${response}. We are unable to retrieve conversion rates.`;
+            errorContainer.innerText = "";
+            errorContainer.append(errorMessage);
+        }
     }
 };
 
@@ -33,21 +42,27 @@ const getConversionTo = async () => {
     const fromAmount = document.getElementById("from-amount");
     const toCurrency = document.getElementById("to-currency").value;
     const toAmount = document.getElementById("to-amount").value;
+    const errorContainer = document.getElementById("error-container");
+    const errorMessage = document.createElement("h3");
 
-    const response = await ConvertCurrency.getConversion(toCurrency);
-
-    if (response.conversion_rates) {
-        const conversionRate = response.conversion_rates[fromCurrency];
-        fromAmount.value = (toAmount * conversionRate);
-    }
-
-    else {
-        console.error("Error in getConversion:", response);
-        const errorContainer = document.getElementById("error-container");
-        const errorMessage = document.createElement("h3");
-        errorMessage.innerText = `${response}. We are unable to retrieve conversion rates.`;
+    if (!fromCurrency || !toCurrency) {
+        errorMessage.innerText = "Please enter to currencies to exchange between.";
         errorContainer.innerText = "";
         errorContainer.append(errorMessage);
+    } else {
+        const response = await ConvertCurrency.getConversion(toCurrency);
+
+        if (response.conversion_rates) {
+            const conversionRate = response.conversion_rates[fromCurrency];
+            fromAmount.value = (toAmount * conversionRate);
+        }
+
+        else {
+            console.error("Error in getConversion:", response);
+            errorMessage.innerText = `${response}. We are unable to retrieve conversion rates.`;
+            errorContainer.innerText = "";
+            errorContainer.append(errorMessage);
+        }
     }
 };
 
