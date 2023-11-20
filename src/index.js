26279027744f878b6ec3463db5347b72eb70f497
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-19 19:23:33 -0800

    start of error handling logic

--- src/index.js
+++ src/index.js
@@ -8,11 +8,37 @@ const getConversion = async () => {
     const fromAmount = document.getElementById("from-amount").value;
     const toCurrency = document.getElementById("to-currency").value;
     const toAmount = document.getElementById("to-amount");
+
     const conversionResponse = await ConvertCurrency.getConversion(fromCurrency);
 
-    const conversionRate = conversionResponse.conversion_rates[toCurrency];
+    if (conversionResponse.conversion_rates) {
+        const conversionRate = conversionResponse.conversion_rates[toCurrency];
+        toAmount.value = (fromAmount * conversionRate);
+    } else {
+        console.log("reached the error")
+        const errorcontainer = document.getElementById("error-container");
+        const errorMessage = document.createElement("h3");
+        errorMessage.append(conversionResponse);
+        errorcontainer.append(errorMessage);
+    }
 
-    toAmount.value = (fromAmount * conversionRate);
+    // try {
+    //     const conversionResponse = await ConvertCurrency.getConversion(fromCurrency);
+
+    //     if (!conversionResponse || conversionResponse.result !== "success") {
+    //         throw new Error(`Error type: ${conversionResponse[3]}. Failed to retrieve conversion rates.`);
+    //     } else {
+    //         const conversionRate = conversionResponse.conversion_rates[toCurrency];
+
+    //         toAmount.value = (fromAmount * conversionRate);
+    //     }
+    // } catch (error) {
+    //     const errorcontainer = document.getElementById("error-container");
+    //     const errorMessage = document.createElement("h3");
+    //     errorMessage.append(error.message);
+    //     errorcontainer.append(errorMessage);
+    // }
+
 };
 
 document.getElementById("from-amount").addEventListener("input", getConversion);
--- src/js/convert-currency.js
+++ src/js/convert-currency.js
@@ -4,8 +4,13 @@ export default class ConvertCurrency {
         try {
             const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
             const responseData = await response.json();
+            if (!response.ok) {
+                const errorMessage = `Error: ${responseData[3]}`;
+                throw new Error(errorMessage);
+            }
             return responseData;
         } catch (error) {
+            console.log(error);
             return error;
         }
     }
