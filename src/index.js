f356ed76282c9a2b76a406f6ad19777eccf7442c
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-19 16:31:49 -0800

    add initial business logic class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
+import ConvertCurrency from './js/scripts';
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,12 @@
-//business logic
+export default class ConvertCurrency {
+
+    static async getConversion(currency) {
+        try {
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+            return error;
+        }
+    }
+}
