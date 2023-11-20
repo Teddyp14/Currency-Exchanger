9bf9d14219e174438d56d5382f6ce5645d0a2421
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-19 18:26:56 -0800

    add initial UI logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
-import ConvertCurrency from './js/scripts';
+import ConvertCurrency from './js/convert-currency';
+
+const getConversion = async () => {
+    const fromCurrency = document.getElementById("from-currency").value;
+    const fromAmount = document.getElementById("from-amount").value;
+    const toCurrency = document.getElementById("to-currency").value;
+    const toAmount = document.getElementById("to-amount");
+    const conversionResponse = await ConvertCurrency.getConversion(fromCurrency);
+
+    const conversionRate = conversionResponse.conversion_rates[toCurrency];
+
+    toAmount.value = (fromAmount * conversionRate);
+};
+
+document.getElementById("from-amount").addEventListener("input", getConversion);
