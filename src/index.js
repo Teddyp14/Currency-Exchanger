8c1e408935d2967796f331c8645fda64247a75bb
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-21 12:54:22 -0800

    add functions to call API when currency codes entered

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
 import ConvertCurrency from './js/convert-currency';
+import GetCurrencies from './js/get-currencies';
 
 const handleError = (response, errorMessage, errorContainer) => {
-    console.error("Error in getConversionFrom:", response);
+    console.error("Error: ", response);
 
     if (response.message.includes("404")) {
         errorMessage.innerText = `${response}. Invalid currency code. Please select from the list provided.`;
@@ -24,7 +25,12 @@ const getVariables = () => {
     return { currencyOne, currencyTwo, errorContainer, errorMessage };
 };
 
-const getConversionFrom = async () => {
+const printError = (errorMessage, errorContainer) => {
+    errorMessage.innerText = "Please enter two currencies to exchange between.";
+    errorContainer.append(errorMessage);
+}
+
+const getConversionOne = async () => {
     const vars = getVariables();
     const amountOne = document.getElementById("amount-one").value;
     const amountTwo = document.getElementById("amount-two");
@@ -32,8 +38,7 @@ const getConversionFrom = async () => {
     vars.errorContainer.innerText = "";
 
     if (!vars.currencyOne || !vars.currencyTwo) {
-        vars.errorMessage.innerText = "Please enter two currencies to exchange between.";
-        vars.errorContainer.append(vars.errorMessage);
+        printError(vars.errorMessage, vars.errorContainer)
 
     } else {
 
@@ -48,7 +53,7 @@ const getConversionFrom = async () => {
     }
 };
 
-const getConversionTo = async () => {
+const getConversionTwo = async () => {
     const vars = getVariables();
     const amountOne = document.getElementById("amount-one");
     const amountTwo = document.getElementById("amount-two").value;
@@ -56,8 +61,7 @@ const getConversionTo = async () => {
     vars.errorContainer.innerText = "";
 
     if (!vars.currencyOne || !vars.currencyTwo) {
-        vars.errorMessage.innerText = "Please enter to currencies to exchange between.";
-        vars.errorContainer.append(vars.errorMessage);
+        printError(vars.errorMessage, vars.errorContainer)
     } else {
         const response = await ConvertCurrency.getConversion(vars.currencyTwo, vars.currencyOne);
 
@@ -71,10 +75,27 @@ const getConversionTo = async () => {
     }
 };
 
+const codeConvertOne = () => {
+    const vars = getVariables();
+
+    if (vars.currencyOne.length === 3) {
+        getConversionOne();
+    }
+}
+
+const codeConvertTwo = () => {
+    const vars = getVariables();
+
+    if (vars.currencyTwo.length === 3) {
+        getConversionTwo();
+    }
+}
+
 const showCurrencies = async () => {
     const currencies = document.getElementById("currencies");
     const currencyList = document.getElementById("currency-list");
-    const response = await ConvertCurrency.getConversion("USD");
+    const response = await GetCurrencies.getCurrencies();
+
 
     Object.keys(response.conversion_rates).forEach((key) => {
         const currency = document.createElement("li");
@@ -85,9 +106,10 @@ const showCurrencies = async () => {
     currencies.append(currencyList);
 };
 
-document.getElementById("amount-one").addEventListener("input", getConversionFrom);
-document.getElementById("amount-two").addEventListener("input", getConversionTo);
+document.getElementById("amount-one").addEventListener("input", getConversionOne);
+document.getElementById("amount-two").addEventListener("input", getConversionTwo);
+document.getElementById("currency-one").addEventListener("input", codeConvertOne);
+document.getElementById("currency-two").addEventListener("input", codeConvertTwo);
 document.getElementById("get-currency").addEventListener("click", showCurrencies);
 
 
-//Need to switch the API to the pair conversion option to get better error handling. Use the standard API to get the list of all currency codes.
---
+++ src/js/get-currencies.js
@@ -0,0 +1,16 @@
+export default class GetCurrencies {
+    static async getCurrencies() {
+        try {
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+            const responseData = await response.json();
+            if (!response.ok) {
+                const errorMessage = `${response.status} ${response.statusText} ${responseData["error-type"]}`;
+                console.log(errorMessage);
+                throw new Error(errorMessage);
+            }
+            return responseData;
+        } catch (error) {
+            return error;
+        }
+    }
+}
